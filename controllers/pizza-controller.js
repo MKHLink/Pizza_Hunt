06d3b5ea4b33fa4da1e7f9f338d29ef9fa12ee41
MKHLink <[likhonhasankomol@gmail.com]>
2022-12-05 16:37:57 -0500

    finished comment model and integrated to frontend

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
 
     getAllPizza(req,res){
         Pizza.find({})
+        .populate({
+          path: 'comments',
+          select: '-__v'
+        })
+        .select('-__v')
+        .sort({_id:-1})
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err =>{
             console.log(err);
@@ -11,19 +17,23 @@ const pizzaController = {
         });
     },
 
-    getPizzaById({params},res){
-        Pizza.findOne({_id: params.id})
-        .then(dbPizzaData =>{
-            if(!dbPizzaData)
-            {
-                res.staus(404).json({message: 'Pizza not found'});
-                return;
-            }
-            res.json(dbPizzaData);
+    getPizzaById({ params }, res) {
+      Pizza.findOne({ _id: params.id })
+        .populate({
+          path: 'comments',
+          select: '-__v'
         })
-        .catch(err=>{
-            console.log(err);
-            res.status(400).json(err);
+        .select('-__v')
+        .then(dbPizzaData => {
+          if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+          }
+          res.json(dbPizzaData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
         });
     },
 
